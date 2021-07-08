01cac14b7a45ffd7d301d68a66584bfd7cc58948
DaleCooper59 <duvinagechristopher1@gmail.com>
2021-07-08 16:57:06 +0200

    CI ---------------- modif

---
+++ application/logs/log-2021-07-07.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-07-07 07:27:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Base 'jarditou' inconnue C:\wamp64\www\CI\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-07-07 07:27:27 --> Unable to connect to the database
